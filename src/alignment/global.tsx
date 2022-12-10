52a75aad6808ad2fb4cbafab0993db1bd941f3da
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-12-10 04:41:37 +0300

    added the alignment functions

---
+++ src/alignment/global.tsx
@@ -0,0 +1,96 @@
+import { Scoring, Alignment, Nucleotide } from '@customTypes/alignment';
+
+interface Props {
+  sequence1: string;
+  sequence2: string;
+  scoring: Scoring;
+};
+
+interface Result {
+  alignMatrix: number[][];
+  alignmentScore: number;
+  optimalAlignments: Alignment[];
+};
+
+export const needlemanWunsch = (props: Props): Result => {
+  const { sequence1: seq1, sequence2: seq2, scoring } = props;
+  const alignMatrix = Array.from(Array(seq1.length + 1), () => new Array(seq2.length + 1));
+
+  alignMatrix[0][0] = 0;
+
+  for (let i = 1; i <= seq1.length; i++) {
+    alignMatrix[i][0] = alignMatrix[i - 1][0] + scoring.gap;
+  }
+
+  for (let j = 1; j <= seq2.length; j++) {
+    alignMatrix[0][j] = alignMatrix[0][j - 1] + scoring.gap;
+  }
+
+  // Compute the alignment score matrix
+  for (let i = 1; i <= seq1.length; i++) {
+    const aChar = seq1[i - 1] as Nucleotide;
+
+    for (let j = 1; j <= seq2.length; j++) {
+      const bChar = seq2[j - 1] as Nucleotide;
+
+      alignMatrix[i][j] = Math.max(
+        alignMatrix[i - 1][j] + scoring.gap, // up
+        alignMatrix[i][j - 1] + scoring.gap, // left
+        alignMatrix[i - 1][j - 1] + scoring.matchOrMismatch[aChar][bChar] // diagonal
+      )
+    }
+  }
+
+  // Backtrack to find all optimal alignments
+  const alignments: Alignment[] = [];
+  const backtrack = (i: number, j: number, seq1Aligned: string, seq2Aligned: string) => {
+    if (i === 0 || j === 0) {
+      while (i !== 0) {
+        seq1Aligned = seq1[i - 1] + seq1Aligned;
+        seq2Aligned = '_' + seq2Aligned;
+        i--;
+      }
+
+      while (j !== 0) {  
+        seq1Aligned = '_' + seq1Aligned;
+        seq2Aligned = seq2[j - 1] + seq2Aligned;
+        j--;
+      }
+
+      alignments.push({
+        seq1Aligned: seq1Aligned,
+        seq2Aligned: seq2Aligned
+      });
+
+      return;
+    }
+
+    const aChar = seq1[i - 1] as Nucleotide;
+    const bChar = seq2[j - 1] as Nucleotide;
+
+    if (alignMatrix[i][j] === (alignMatrix[i - 1][j - 1] + scoring.matchOrMismatch[aChar][bChar])) {
+      // diagonal path
+      backtrack(i - 1, j - 1, seq1[i - 1] + seq1Aligned, seq2[j - 1] + seq2Aligned);
+    }
+    if (alignMatrix[i][j] === alignMatrix[i - 1][j] + scoring.gap) {
+      // up path
+      backtrack(i - 1, j, seq1[i - 1] + seq1Aligned, '_' + seq2Aligned);
+    }
+    if (alignMatrix[i][j] === alignMatrix[i][j - 1] + scoring.gap) {
+      // left path
+      backtrack(i, j - 1, '_' + seq1Aligned, seq2[j - 1] + seq2Aligned);
+    }
+  }
+
+  const i = seq1.length;
+  const j = seq2.length;
+  const seq1Aligned = '';
+  const seq2Aligned = '';
+  backtrack(i, j, seq1Aligned, seq2Aligned);
+
+  return {
+    alignMatrix: alignMatrix,
+    alignmentScore: alignMatrix[seq1.length][seq2.length],
+    optimalAlignments: alignments
+  };
+}
---
+++ src/alignment/local.tsx
@@ -0,0 +1,105 @@
+import { Scoring, Alignment, Nucleotide } from '@customTypes/alignment';
+
+interface Props {
+  sequence1: string;
+  sequence2: string;
+  scoring: Scoring;
+};
+
+interface Result {
+  alignMatrix: number[][];
+  alignmentScore: number;
+  optimalAlignments: Alignment[];
+};
+
+export const watermanSmith = (props: Props): Result => {
+  const { sequence1: seq1, sequence2: seq2, scoring } = props;
+  const alignMatrix = Array.from(Array(seq1.length + 1), () => new Array(seq2.length + 1));
+
+  alignMatrix[0][0] = 0;
+
+  for (let i = 1; i <= seq1.length; i++) {
+    alignMatrix[i][0] = 0;
+  }
+
+  for (let j = 1; j <= seq2.length; j++) {
+    alignMatrix[0][j] = 0;
+  }
+
+  // Compute the alignment score matrix
+  for (let i = 1; i <= seq1.length; i++) {
+    const aChar = seq1[i - 1] as Nucleotide;
+
+    for (let j = 1; j <= seq2.length; j++) {
+      const bChar = seq2[j - 1] as Nucleotide;
+
+      alignMatrix[i][j] = Math.max(
+        alignMatrix[i - 1][j] + scoring.gap, // up
+        alignMatrix[i][j - 1] + scoring.gap, // left
+        alignMatrix[i - 1][j - 1] + scoring.matchOrMismatch[aChar][bChar], // diagonal
+        0
+      )
+    }
+  }
+
+  // Find the maximum alignment score
+  let score = 0;
+  let scoreIndexes: number[][] = [];
+
+  for (let i = 1; i <= seq1.length; i++) {
+    for (let j = 1; j <= seq2.length; j++) {
+      if (alignMatrix[i][j] > score) {
+        score = alignMatrix[i][j];
+        scoreIndexes = [[i, j]];
+      } else if (alignMatrix[i][j] === score) {
+        scoreIndexes.push([i, j]);
+      }
+    }
+  }
+
+  // Backtrack to find all optimal alignments
+  const alignments: Alignment[] = [];
+
+  for (const scoreIndex of scoreIndexes) {
+    let countdown = score;
+    let i = scoreIndex[0];
+    let j = scoreIndex[1];
+    let seq1Aligned = '';
+    let seq2Aligned = '';
+
+    while (countdown > 0) {
+      const aChar = seq1[i - 1] as Nucleotide;
+      const bChar = seq2[j - 1] as Nucleotide;
+
+      if (alignMatrix[i][j] === (alignMatrix[i - 1][j - 1] + scoring.matchOrMismatch[aChar][bChar])) {
+        // diagonal path
+        seq1Aligned = seq1[i - 1] + seq1Aligned;
+        seq2Aligned = seq2[j - 1] + seq2Aligned;
+        i--;
+        j--;
+        countdown = alignMatrix[i][j];
+      } else if (alignMatrix[i][j] === alignMatrix[i - 1][j] + scoring.gap) {
+        // up path
+        seq1Aligned = seq1[i - 1] + seq1Aligned;
+        seq2Aligned = '_' + seq2Aligned;
+        i--;
+      } else {
+        // left path
+        seq1Aligned = '_' + seq1Aligned;
+        seq2Aligned = seq2[j - 1] + seq2Aligned;
+        j--;
+      }
+    }
+
+    alignments.push({
+      seq1Aligned: seq1Aligned,
+      seq2Aligned: seq2Aligned
+    });
+  }
+
+  return {
+    alignMatrix: alignMatrix,
+    alignmentScore: score,
+    optimalAlignments: alignments
+  };
+}
