8129546562f0cbdb2bdad07d5e76eb4daeb810f0
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-12-12 12:37:03 +0300

    MUI TextField

---
+++ src/components/CustomTextField.tsx
@@ -0,0 +1,45 @@
+import { TextField, TextFieldProps } from '@mui/material';
+import {
+  Control,
+  Controller,
+  Path,
+  FieldValues,
+} from 'react-hook-form';
+
+export type CustomTextFieldProps<T extends FieldValues> = Omit<TextFieldProps, 'name'> & {
+  name: Path<T>;
+  control: Control<T, any>;
+};
+
+export const CustomTextField = <T extends FieldValues>(props: CustomTextFieldProps<T>) => {
+  const {
+    name,
+    control,
+    type,
+    helperText,
+    ...rest
+  } = props;
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { onChange, value, ...field }, fieldState: { error } }) => (
+        <TextField
+          type={type}
+          onChange={(ev) => {
+            onChange(ev);
+            if (typeof rest.onChange === 'function') {
+              rest.onChange(ev);
+            }
+          }}
+          value={value || ''}
+          error={!!error}
+          helperText={error ? error.message : helperText}
+          {...rest}
+          {...field}
+        />
+      )}
+    />
+  );
+};
