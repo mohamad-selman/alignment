955bc8a994ce1a44957f2e5f665d83fa1b0d4b29
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-12-23 16:15:18 +0300

    separate component for form

---
+++ src/components/InputForm.tsx
@@ -0,0 +1,92 @@
+import { Box, Switch, FormControlLabel } from '@mui/material';
+import { CustomTextField } from '@src/components/CustomTextField';
+import { Dispatch, SetStateAction } from 'react';
+import { Control } from 'react-hook-form';
+
+type FormValues = {
+  seq1: string;
+  seq2: string;
+  match: string;
+  mismatch: string;
+  gapPenalty: string;
+  AA: string;
+  AC: string;
+  AG: string;
+  AT: string;
+  CC: string;
+  CG: string;
+  CT: string;
+  GG: string;
+  GT: string;
+  TT: string;
+}
+
+interface Props {
+  control: Control<FormValues, any>;
+  matrixSwitch: boolean;
+  setMatrixSwitch: Dispatch<SetStateAction<boolean>>;
+}
+
+const InputForm = (props: Props) => {
+  const { control, matrixSwitch, setMatrixSwitch } = props;
+
+  return (
+    <form noValidate>
+      <Box sx={{ display: 'flex', flexDirection: 'column', rowGap: '2rem' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
+          <CustomTextField
+            name='seq1'
+            control={control}
+            label='First Sequence'
+          />
+          <CustomTextField
+            name='seq2'
+            control={control}
+            label='Second Sequence'
+          />
+        </Box>
+        <Box sx={{
+          display: 'flex', flexDirection: 'row', columnGap: '2rem', justifyContent: 'space-between',
+        }}>
+          {matrixSwitch
+            ? <Box sx={{ display: 'flex', flexDirection: 'column', rowGap: '0.75rem' }}>
+                <Box>
+                  <CustomTextField name='gapPenalty' type='number' control={control} label='Gap Penalty' />
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
+                  <CustomTextField name='AA' type='number' control={control} label='A-A'/>
+                  <CustomTextField name='AC' type='number' control={control} label='A-C'/>
+                  <CustomTextField name='AG' type='number' control={control} label='A-G'/>
+                  <CustomTextField name='AT' type='number' control={control} label='A-T'/>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
+                  <CustomTextField name='CC' type='number' control={control} label='C-C'/>
+                  <CustomTextField name='CG' type='number' control={control} label='C-G'/>
+                  <CustomTextField name='CT' type='number' control={control} label='C-T'/>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
+                  <CustomTextField name='GG' type='number' control={control} label='G-G'/>
+                  <CustomTextField name='GT' type='number' control={control} label='G-T'/>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
+                  <CustomTextField name='TT' type='number' control={control} label='T-T'/>
+                </Box>
+              </Box>
+            : <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
+                <CustomTextField name='match' type='number' control={control} label='Match Score' />
+                <CustomTextField name='mismatch' type='number' control={control} label='Mismatch Score' />
+                <CustomTextField name='gapPenalty' type='number' control={control} label='Gap Penalty' />
+              </Box>
+          }
+          <Box>
+            <FormControlLabel control={
+              <Switch checked={matrixSwitch} onChange={() => setMatrixSwitch(!matrixSwitch)} />
+            } label='Matrix' />
+          </Box>
+        </Box>
+      </Box>
+    </form>
+  );
+};
+
+export default InputForm;
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -2,14 +2,8 @@ import navLinks from '@constants/navLinks';
 import needlemanWunsch from '@src/alignment/global';
 import { Result } from '@customTypes/alignment';
 import Layout from '@components/Layout';
-import {
-  Typography,
-  Box,
-  Divider,
-  Switch,
-  FormControlLabel,
-} from '@mui/material';
-import { CustomTextField } from '@src/components/CustomTextField';
+import { Typography, Divider } from '@mui/material';
+import InputForm from '@src/components/InputForm';
 import AlignmentResults from '@src/components/AlignmentResults';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -147,61 +141,11 @@ const Test = () => {
       <Typography variant='h4' sx={{ mb: 5 }} color='secondary.main'>
         Needleman-Wunsch Algorithm
       </Typography>
-      <form noValidate>
-        <Box sx={{ display: 'flex', flexDirection: 'column', rowGap: '2rem' }}>
-          <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
-            <CustomTextField
-              name='seq1'
-              control={control}
-              label='First Sequence'
-            />
-            <CustomTextField
-              name='seq2'
-              control={control}
-              label='Second Sequence'
-            />
-          </Box>
-          <Box sx={{
-            display: 'flex', flexDirection: 'row', columnGap: '2rem', justifyContent: 'space-between',
-          }}>
-            {matrixSwitch
-              ? <Box sx={{ display: 'flex', flexDirection: 'column', rowGap: '0.75rem' }}>
-                  <Box>
-                    <CustomTextField name='gapPenalty' type='number' control={control} label='Gap Penalty' />
-                  </Box>
-                  <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
-                    <CustomTextField name='AA' type='number' control={control} label='A-A'/>
-                    <CustomTextField name='AC' type='number' control={control} label='A-C'/>
-                    <CustomTextField name='AG' type='number' control={control} label='A-G'/>
-                    <CustomTextField name='AT' type='number' control={control} label='A-T'/>
-                  </Box>
-                  <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
-                    <CustomTextField name='CC' type='number' control={control} label='C-C'/>
-                    <CustomTextField name='CG' type='number' control={control} label='C-G'/>
-                    <CustomTextField name='CT' type='number' control={control} label='C-T'/>
-                  </Box>
-                  <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
-                    <CustomTextField name='GG' type='number' control={control} label='G-G'/>
-                    <CustomTextField name='GT' type='number' control={control} label='G-T'/>
-                  </Box>
-                  <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
-                    <CustomTextField name='TT' type='number' control={control} label='T-T'/>
-                  </Box>
-                </Box>
-              : <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: '0.75rem' }}>
-                  <CustomTextField name='match' type='number' control={control} label='Match Score' />
-                  <CustomTextField name='mismatch' type='number' control={control} label='Mismatch Score' />
-                  <CustomTextField name='gapPenalty' type='number' control={control} label='Gap Penalty' />
-                </Box>
-            }
-            <Box>
-              <FormControlLabel control={
-                <Switch checked={matrixSwitch} onChange={() => setMatrixSwitch(!matrixSwitch)} />
-              } label='Matrix' />
-            </Box>
-          </Box>
-        </Box>
-      </form>
+      <InputForm
+        control={control}
+        matrixSwitch={matrixSwitch}
+        setMatrixSwitch={setMatrixSwitch}
+      />
       <Divider sx={{ py: 4 }}>Results</Divider>
       {isValid && !isValidating && result && <AlignmentResults result={result} />}
     </Layout>
