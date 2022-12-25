24dfb0108bde97a4ac2cc8dec51ff99d7a307182
Mohamad Salman <Mo7amadsalman@hotmail.com>
2022-12-25 18:22:14 +0300

    Fixed a bug in watermanSmith

--- src/alignment/local.ts
+++ src/alignment/local.ts
@@ -41,77 +41,53 @@ const watermanSmith = (props: Props): Result => {
     }
   }
 
-  // Find the maximum alignment score
-  let score = 0;
-  let scoreIndexes: number[][] = [];
-
-  for (let i = 1; i <= seq1.length; i += 1) {
-    for (let j = 1; j <= seq2.length; j += 1) {
-      if (alignMatrix[i][j] > score) {
-        score = alignMatrix[i][j];
-        scoreIndexes = [[i, j]];
-      } else if (alignMatrix[i][j] === score) {
-        scoreIndexes.push([i, j]);
-      }
-    }
-  }
-
   // Backtrack to find all optimal alignments
   const alignments: Alignment[] = [];
+  // eslint-disable-next-line max-len
+  const backtrack = (i: number, j: number, seq1Aligned: string, seq2Aligned: string, path: number[][]) => {
+    path.push([i, j]);
 
-  for (let idx = 0; idx < scoreIndexes.length; idx += 1) {
-    const scoreIndex = scoreIndexes[idx];
+    if (i === 0 || j === 0 || alignMatrix[i][j] === 0) {
+      alignments.push({
+        seq1Aligned,
+        seq2Aligned,
+        path,
+      });
 
-    let countdown = score;
-    let i = scoreIndex[0];
-    let j = scoreIndex[1];
-    let seq1Aligned = '';
-    let seq2Aligned = '';
-    const path: number[][] = [scoreIndex];
-
-    while (countdown > 0) {
-      const aChar = seq1[i - 1] as Nucleotide;
-      const bChar = seq2[j - 1] as Nucleotide;
-
-      // eslint-disable-next-line max-len
-      if (alignMatrix[i][j] === (alignMatrix[i - 1][j - 1] + scoring.matchOrMismatch[aChar][bChar])) {
-        // diagonal path
-        seq1Aligned = seq1[i - 1] + seq1Aligned;
-        seq2Aligned = seq2[j - 1] + seq2Aligned;
-        i -= 1;
-        j -= 1;
-        countdown = alignMatrix[i][j];
-
-        path.push([i, j]);
-      } else if (alignMatrix[i][j] === alignMatrix[i - 1][j] + scoring.gap) {
-        // up path
-        seq1Aligned = seq1[i - 1] + seq1Aligned;
-        seq2Aligned = `—${seq2Aligned}`;
-        i -= 1;
-
-        path.push([i, j]);
-      } else {
-        // left path
-        seq1Aligned = `—${seq1Aligned}`;
-        seq2Aligned = seq2[j - 1] + seq2Aligned;
-        j -= 1;
-
-        path.push([i, j]);
-      }
+      return;
     }
 
-    alignments.push({
-      seq1Aligned,
-      seq2Aligned,
-      path,
-    });
+    const aChar = seq1[i - 1] as Nucleotide;
+    const bChar = seq2[j - 1] as Nucleotide;
+
+    if (alignMatrix[i][j] === (alignMatrix[i - 1][j - 1] + scoring.matchOrMismatch[aChar][bChar])) {
+      // diagonal path
+      backtrack(i - 1, j - 1, seq1[i - 1] + seq1Aligned, seq2[j - 1] + seq2Aligned, [...path]);
+    }
+    if (alignMatrix[i][j] === alignMatrix[i - 1][j] + scoring.gap) {
+      // up path
+      backtrack(i - 1, j, seq1[i - 1] + seq1Aligned, `—${seq2Aligned}`, [...path]);
+    }
+    if (alignMatrix[i][j] === alignMatrix[i][j - 1] + scoring.gap) {
+      // left path
+      backtrack(i, j - 1, `—${seq1Aligned}`, seq2[j - 1] + seq2Aligned, [...path]);
+    }
+  };
+
+  const maxScore = Math.max(...alignMatrix.map((row) => Math.max(...row)));
+  for (let i = 1; i <= seq1.length; i += 1) {
+    for (let j = 1; j <= seq2.length; j += 1) {
+      if (alignMatrix[i][j] === maxScore) {
+        backtrack(i, j, '', '', []);
+      }
+    }
   }
 
   return {
     seq1,
     seq2,
     alignMatrix,
-    alignmentScore: score,
+    alignmentScore: maxScore,
     optimalAlignments: alignments,
   };
 };
